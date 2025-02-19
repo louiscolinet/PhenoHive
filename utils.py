b5d9b6ae1eeb5c18af76d6bbab3da69cb693cd42
louiscolinet <61157222+louiscolinet@users.noreply.github.com>
2025-02-19 15:28:57 +0100

    setup_logger

--- utils.py
+++ utils.py
@@ -2,16 +2,17 @@ import datetime
 import logging
 
 
-def setup_logger(name: str, level: str) -> logging.Logger:
+def setup_logger(name: str, level: str | int, folder_path: str) -> logging.Logger:
     """
     Function to set up the logger
     :param name: name of the logger
-    :param level: logging level, can be DEBUG, INFO, WARNING, ERROR, CRITICAL
+    :param level: logging level, can be DEBUG (10), INFO (20), WARNING (30), ERROR (40), CRITICAL (50)
+    :param folder_path: path to the folder where the logs will be saved
     :return: logger object
     """
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     date = datetime.datetime.now().strftime("%Y-%m-%d")
-    log_file = f"logs/{date}_{name}.log"
+    log_file = f"{folder_path}/{date}_{name}.log"
     handler = logging.FileHandler(log_file)
     handler.setFormatter(formatter)
 
