5bb63f7e81df9c9859d5b06dbfdfe4a84cd2efb9
louiscolinet <61157222+louiscolinet@users.noreply.github.com>
2025-03-04 11:55:33 +0100

    change sigma edges_crop

--- image_processing.py
+++ image_processing.py
@@ -31,7 +31,7 @@ def get_height_pix(image_path: str, pot_limit: int, channel: str = 'k', kernel_s
     k = pcv.rgb2gray_cmyk(rgb_img=img, channel=channel)
     k_mblur = pcv.median_blur(k, kernel_size)
 
-    edges = pcv.canny_edge_detect(k_mblur, sigma=2)
+    edges = pcv.canny_edge_detect(k_mblur, sigma=1)
     edges_crop = pcv.crop(edges, 5, 5, height - pot_limit - 10, width - 10)
     new_height = edges_crop.shape[0]
     edges_filled = pcv.fill(edges_crop, fill_size)
