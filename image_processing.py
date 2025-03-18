4b250c2c61165fddaba54d8564b58a5bf8740acc
louiscolinet <61157222+louiscolinet@users.noreply.github.com>
2025-03-18 10:30:29 +0100

    Update image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -78,7 +78,7 @@ def get_segment_list(image_path: str, channel: str = 'k', kernel_size: int = 20,
     cv2.imwrite("data/closing.jpg", closing)
 
     # Find contours
-    thresh = cv2.threshold(closing, 128, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.adaptiveThreshold(closing, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
     contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = contours[0]
     big_contour = max(contours, key=cv2.contourArea)
