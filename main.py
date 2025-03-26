40a31357a6b1209f21edda5fc38cd91ee8f6dc99
louiscolinet <61157222+louiscolinet@users.noreply.github.com>
2025-03-26 12:39:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -115,7 +115,6 @@ def handle_imag_param_calibration_menu(station: PhenoHiveStation) -> None:
     .......
     :param station: station object
     """
-    time.sleep(1)
     calib_test_num = 0
     sigma = float(station.parser['image_arg']["sigma"])
     kernel_size = int(station.parser['image_arg']["kernel_size"])
@@ -123,10 +122,10 @@ def handle_imag_param_calibration_menu(station: PhenoHiveStation) -> None:
     while True:
         if not GPIO.input(station.BUT_RIGHT):
             time.sleep(1)
-            if calib_test_num > 10:
-                break
-            calib_test_num += 1
+            """if calib_test_num > 10:
+                break"""
             station.disp.show_img_param_menu(sigma, kernel_size, calib_test_num)
+            calib_test_num += 1
 
         if not GPIO.input(station.BUT_LEFT):
             time.sleep(1)
