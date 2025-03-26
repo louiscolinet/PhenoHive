2e629000acfa0db00c0c11318ffdc69513ca5250
louiscolinet <61157222+louiscolinet@users.noreply.github.com>
2025-03-26 14:18:02 +0100

    Update affichage

--- show_display.py
+++ show_display.py
@@ -133,7 +133,7 @@ class Display:
         draw.text((24, 80), "Calibration", font=font, fill=(0, 0, 0))
         # Button
         font = ImageFont.truetype(FONT, 10)
-        draw.text((0, 130), "<-- Weight      Img -->", font=font, fill=(0, 0, 0))
+        draw.text((0, 130), "<-- Weight       Img -->", font=font, fill=(0, 0, 0))
         self.SCREEN.display(img)
 
     def show_weight_cal_menu(self, raw_weight, weight_g, tare, calib_or_test) -> None:
