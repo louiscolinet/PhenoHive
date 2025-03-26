e48608b1f93fad68c4c41b178a1cfda6ffa5333c
louiscolinet <61157222+louiscolinet@users.noreply.github.com>
2025-03-26 14:24:50 +0100

    debug

--- show_display.py
+++ show_display.py
@@ -168,10 +168,10 @@ class Display:
         """
         img, draw = self.create_image(logo=True)
         # Button
-        """font = ImageFont.truetype(FONT, 10)
+        font = ImageFont.truetype(FONT, 10)
         draw.text((0, 80), f"Sigma value: {sigma}", font=font, fill=(0, 0, 0))
         draw.text((0, 95), f"Kernel size value: {kernel_size}", font=font, fill=(0, 0, 0))
-        draw.text((0, 130), f"<-- Calib {calib_test_num}         Back -->", font=font, fill=(0, 0, 0))"""
+        draw.text((0, 130), f"<-- Calib {calib_test_num}         Back -->", font=font, fill=(0, 0, 0))
         self.SCREEN.display(img)
 
     def show_collecting_data(self, action):
