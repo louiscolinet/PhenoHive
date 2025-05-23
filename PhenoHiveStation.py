1dab8b675aef67dc56c4e539710864bb56a24f1e
louiscolinet <61157222+louiscolinet@users.noreply.github.com>
2025-05-23 15:19:05 +0200

    Update PhenoHiveStation.py

--- PhenoHiveStation.py
+++ PhenoHiveStation.py
@@ -609,7 +609,7 @@ class PhenoHiveStation:
         
         analog_voltage = self.mcp.read_adc(self.HUM) * (3.3 / 1023.0)
         GPIO.output(self.EN_HUM, GPIO.LOW)
-        hum = 1/0.125 * np.tan((2.022-analog_voltage)/0.97) + 4.802
+        hum = (np.tan((2.041-analog_voltage)/0.994) + 4.515) / 0.119 
         if hum < 0: hum = 0.0
         return hum
 
