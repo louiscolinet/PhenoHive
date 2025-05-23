fe23d34deea60980c273419ae2e0ba20f4c07db9
louiscolinet <61157222+louiscolinet@users.noreply.github.com>
2025-05-23 10:14:08 +0200

    oups

--- PhenoHiveStation.py
+++ PhenoHiveStation.py
@@ -604,11 +604,11 @@ class PhenoHiveStation:
 
     def humidity_pipeline(self):
         self.disp.show_collecting_data("Measuring humidity")
-        GPIO.output(self.LED, GPIO.LOW)
+        GPIO.output(self.EN_HUM, GPIO.LOW)
         time.sleep(0.5)
         
         analog_voltage = self.mcp.read_adc(self.HUM) * (3.3 / 1023.0)
-        GPIO.output(self.LED, GPIO.HIGH)
+        GPIO.output(self.EN_HUM, GPIO.HIGH)
         hum = 1/0.125 * np.tan((2.022-analog_voltage)/0.97) + 4.802
         if hum < 0: hum = 0.0
         return hum
