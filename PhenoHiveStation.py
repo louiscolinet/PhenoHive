bf4781e2407385f1bb297f1852d11aedfbcc77bf
louiscolinet <61157222+louiscolinet@users.noreply.github.com>
2025-06-09 22:52:44 +0200

    test led

--- PhenoHiveStation.py
+++ PhenoHiveStation.py
@@ -197,12 +197,14 @@ class PhenoHiveStation:
         GPIO.setwarnings(False)
         GPIO.setup(self.LED, GPIO.OUT)
         GPIO.output(self.LED, GPIO.LOW)
-        time.sleep(0.7)
+        time.sleep(1)
         GPIO.output(self.LED, GPIO.HIGH)
-        time.sleep(0.7)
+        time.sleep(1)
         GPIO.output(self.LED, GPIO.LOW)
-        time.sleep(0.7)
+        time.sleep(1)
         GPIO.output(self.LED, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(self.LED, GPIO.LOW)
         
     def init_load(self):
         # Hx711
