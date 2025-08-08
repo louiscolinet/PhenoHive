efa5613ff8f8344a25d82112963bf4260c66d51f
louiscolinet <61157222+louiscolinet@users.noreply.github.com>
2025-08-08 18:53:13 +0200

    ajout prune

--- image_processing.py
+++ image_processing.py
@@ -93,7 +93,7 @@ def get_segment_list(image_path: str, channel: str = 'k', kernel_size: int = 20,
     # Draw plant skeleton and segment
     pcv.params.line_thickness = 3
     skeleton = pcv.morphology.skeletonize(mask=result)
-    segmented_img, obj = pcv.morphology.segment_skeleton(skel_img=skeleton)
+    skeleton ,segmented_img, obj = pcv.morphology.prune(skel_img=skeleton, size=20)
   
     if skeleton_filename == None : skeleton_path = "data/images/skeleton.jpg"
     else : skeleton_path = "data/images/" + skeleton_filename
