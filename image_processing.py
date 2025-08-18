658bb346b6b06a3be7a5885f720241f7096c21aa
louiscolinet <61157222+louiscolinet@users.noreply.github.com>
2025-08-18 11:44:13 +0200

    changement parametre shadow + lavage get segment list

--- image_processing.py
+++ image_processing.py
@@ -42,7 +42,7 @@ def get_height_pix(image_path: str, pot_limit: int, channel: str = 'k', kernel_s
     return plant_height_pix
 
 
-def match_luminance(img: np.ndarray, background: np.ndarray, radius: int = 30) -> np.ndarray:
+def match_luminance(img: np.ndarray, background: np.ndarray, radius: int = 150) -> np.ndarray:
     """
     Adapts the background luminance to match the local luminance
     around the brightest pixel in the input image.
@@ -71,7 +71,7 @@ def match_luminance(img: np.ndarray, background: np.ndarray, radius: int = 30) -
     back_yuv[:, :, 0] = back_y_norm
     return cv2.cvtColor(back_yuv, cv2.COLOR_YCrCb2RGB)
 
-def remove_shadows(img: np.ndarray, beta1=0.3, beta2=0.9, tau_s=0.27, tau_h=1) -> np.ndarray:
+def remove_shadows(img: np.ndarray, beta1=0.38, beta2=0.85, tau_s=0.27, tau_h=1) -> np.ndarray:
     """
     Detect and remove shadows using HSV chromacity-based method.
     Based on Sanin et al. (2012), adapted from Cucchiara et al. (2003).
@@ -82,10 +82,8 @@ def remove_shadows(img: np.ndarray, beta1=0.3, beta2=0.9, tau_s=0.27, tau_h=1) -
     """
     img_hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV).astype(np.float32)
 
-    background = cv2.imread("data/images/background.jpg")  # ou autre chemin
-    print("entrée lum")
+    background = cv2.imread("data/images/background.jpg")
     background = match_luminance(img, background)
-    print("sortie lum")
     background = cv2.cvtColor(background, cv2.COLOR_BGR2RGB)
     back_hsv = cv2.cvtColor(background, cv2.COLOR_RGB2HSV).astype(np.float32)
 
@@ -104,26 +102,26 @@ def remove_shadows(img: np.ndarray, beta1=0.3, beta2=0.9, tau_s=0.27, tau_h=1) -
 
     # Detect shadows
     shadow_mask = (v_ratio >= beta1) & (v_ratio <= beta2) & (s_diff <= tau_s * 255) & (h_diff <= tau_h * 255)
-
     shadow_mask_uint8 = shadow_mask.astype(np.uint8) * 255
-    shadow_mask_blurred = cv2.GaussianBlur(shadow_mask_uint8, (3, 3), 0)
+    shadow_mask_blurred = cv2.GaussianBlur(shadow_mask_uint8, (5, 71), 0)
     _, shadow_mask_clean = cv2.threshold(shadow_mask_blurred, 127, 255, cv2.THRESH_BINARY)
     shadow_mask = shadow_mask_clean.astype(bool)
 
     nb_white_pixels = np.count_nonzero(shadow_mask)
 
-    if nb_white_pixels > 5000000:
+    if nb_white_pixels > 20000:
         img_no_shadow = img.copy()
         img_no_shadow[shadow_mask] = background[shadow_mask]
 
-        # blur
+        # Blur
         shadow_mask_u8 = (shadow_mask.astype(np.uint8) * 255)
-        dilated_mask = cv2.dilate(shadow_mask_u8, np.ones((5, 5), np.uint8), iterations=1)
-        transition_mask = cv2.subtract(dilated_mask, shadow_mask_u8)
-        blurred = cv2.GaussianBlur(img_no_shadow, (9, 9), 0)
+        eroded_mask = cv2.erode(shadow_mask_u8, np.ones((60, 60), np.uint8), iterations=1)
+        dilated_mask = cv2.dilate(shadow_mask_u8, np.ones((7, 7), np.uint8), iterations=1)
+        transition_mask = cv2.subtract(dilated_mask, eroded_mask)
+        blurred = cv2.GaussianBlur(img_no_shadow, (5, 91), 0)
         img_no_shadow[transition_mask.astype(bool)] = blurred[transition_mask.astype(bool)]
 
-        img_no_shadow = cv2.bilateralFilter(img_no_shadow, d=30, sigmaColor=45, sigmaSpace=95)
+        img_no_shadow = cv2.bilateralFilter(img_no_shadow, d=15, sigmaColor=35, sigmaSpace=95)
     else:
         img_no_shadow = img.copy()
 
@@ -143,12 +141,10 @@ def get_segment_list(image_path: str, channel: str = 'k', kernel_size: int = 20,
 
     # Read image
     img, _, _ = pcv.readimage(image_path)
-    cv2.imwrite("data/img.jpg", img)
 
-    # Remove shadow if any
-    print("entrée_shad")
+    # Remove shadow
     img_no_shadow = remove_shadows(img)
-    print("sortie shad")
+  
     # Get image dimension
     height, width = img_no_shadow.shape[0], img.shape[1]
 
@@ -160,14 +156,11 @@ def get_segment_list(image_path: str, channel: str = 'k', kernel_size: int = 20,
 
     # Crop image edges
     edges_crop = pcv.crop(edges, 5, 5, height - 10, width - 10)
-    #cv2.imwrite("data/edges_crop.jpg", edges_crop)
     crop = pcv.crop(img, 5, 5, height - 10, width - 10)
-    cv2.imwrite("data/crop.jpg", crop)
 
     # Close gaps in plant contour
     kernel = np.ones((kernel_size, kernel_size), np.uint8)
     closing = cv2.morphologyEx(edges_crop, cv2.MORPH_CLOSE, kernel)
-    #cv2.imwrite("data/closing.jpg", closing)
 
     # Find contours
     thresh = cv2.threshold(closing, 128, 255, cv2.THRESH_BINARY)[1]
@@ -180,7 +173,6 @@ def get_segment_list(image_path: str, channel: str = 'k', kernel_size: int = 20,
     # Fill contour to get maize shape
     result = np.zeros_like(closing)
     cv2.drawContours(result, [big_contour], 0, (255, 255, 255), cv2.FILLED)
-    cv2.imwrite("data/result.jpg", result)
 
     # Draw plant skeleton and segment
     pcv.params.line_thickness = 3
@@ -190,9 +182,6 @@ def get_segment_list(image_path: str, channel: str = 'k', kernel_size: int = 20,
     if skeleton_filename == None : skeleton_path = "data/images/skeleton.jpg"
     else : skeleton_path = "data/images/" + skeleton_filename
     cv2.imwrite(skeleton_path, skeleton)
-  
-    cv2.imwrite("data/skeleton.jpg", skeleton)
-    #cv2.imwrite("data/segmented_img.jpg", segmented_img)
 
     _ = pcv.morphology.segment_path_length(segmented_img=segmented_img,
                                                objects=obj, label="default")
